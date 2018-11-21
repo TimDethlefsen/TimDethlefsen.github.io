be472a27cb6f3b011d25c39cee07e745be63b6e3
Tim Dethlefsen <dethlefsentim@gmail.com>
2018-11-21 22:33:52 +0800

    first deployment

--- service_funnel/src/App.vue
+++ service_funnel/src/App.vue
@@ -1,189 +1,27 @@
 <template>
 <div id="app">
+  <Nav></Nav>
 
-  <section class="hero is-info is-fullheight">
-    <!-- Hero head: will stick at the top -->
-    <div class="hero-head">
-      <header class="navbar">
-        <div class="container">
-          <div class="navbar-brand">
-            <a class="navbar-item">
-              <div class="">
-                <b>BRANDNAME</b>
-              </div>
-            </a>
-            <span class="navbar-burger burger" data-target="navbarMenuHeroC">
-              <span></span>
-              <span></span>
-              <span></span>
-            </span>
-          </div>
-          <div id="navbarMenuHeroC" class="navbar-menu">
-            <div class="navbar-end">
-              <a class="navbar-item">
-                Services
-              </a>
-              <a class="navbar-item">
-                Why us
-              </a>
-              <span class="navbar-item">
-                <a class="button is-info is-inverted">
-                  <span>For Service Partner</span>
-                </a>
-              </span>
-            </div>
-          </div>
-        </div>
-      </header>
-    </div>
+  <Home></Home>
 
-    <!-- Hero content: will be in the middle -->
-    <div class="hero-body ">
-      <div class="container has-text-centered">
-        <h1 class="title">
-          Find out how much Money your Company can save with our excellent Service
-        </h1>
-        <router-view></router-view>
-      </div>
-    </div>
-  </section>
-  <div class="container ">
-    <br />
-    <section>
-      <div class="tile is-ancestor has-text-centered">
+  <Footer></Footer>
 
-        <div class="tile">
-          <div class="tile is-parent is-vertical">
-            <article class="tile is-child notification is-light">
-              <p class="title">Vertical...</p>
-              <p class="subtitle">Top</p>
-            </article>
-            <article class="tile is-child notification is-light">
-              <p class="title">...tiles</p>
-              <p class="subtitle">Bottom tile</p>
-            </article>
-          </div>
-          <div class="tile is-parent is-vertical">
-            <article class="tile is-child notification is-light">
-              <p class="title">Vertical...</p>
-              <p class="subtitle">Top tile</p>
-            </article>
-            <article class="tile is-child notification is-light">
-              <p class="title">...tiles</p>
-              <p class="subtitle">Bottom tile</p>
-            </article>
-          </div>
-          <div class="tile is-parent is-vertical">
-            <article class="tile is-child notification is-light">
-              <p class="title">Vertical...</p>
-              <p class="subtitle">Top tile</p>
-            </article>
-            <article class="tile is-child notification is-light">
-              <p class="title">...tiles</p>
-              <p class="subtitle">Bottom tile</p>
-            </article>
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
-  <br />
-
-  <section>
-    <nav class="level">
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Tweets</p>
-          <p class="title">3,456</p>
-        </div>
-      </div>
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Following</p>
-          <p class="title">123</p>
-        </div>
-      </div>
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Followers</p>
-          <p class="title">456K</p>
-        </div>
-      </div>
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Likes</p>
-          <p class="title">789</p>
-        </div>
-      </div>
-    </nav>
-
-  </section>
-  <br />
-
-  <div class="container ">
-    <section>
-      <div class="columns is-multiline">
-        <div class="column is-four-fifths">
-          <article class="media">
-            <figure class="media-left">
-              <p class="image is-256x256">
-                <img src="https://bulma.io/images/placeholders/256x256.png">
-              </p>
-            </figure>
-            <div class="media-content has-text-left">
-              <p class="title">
-                “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”
-              </p>
-              <p class="subtitle">
-                Jeff Atwood
-              </p>
-            </div>
-          </article>
-        </div>
-        <div class="column is-four-fifths is-offset-one-fifth">
-          <article class="media">
-
-            <div class="media-content has-text-right">
-              <p class="title">
-                “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”
-              </p>
-              <p class="subtitle">
-                Jeff Atwood
-              </p>
-            </div>
-            <figure class="media-left">
-              <p class="image is-256x256">
-                <img src="https://bulma.io/images/placeholders/256x256.png">
-              </p>
-            </figure>
-          </article>
-        </div>
-
-      </div>
-    </section>
-  </div>
-
-
-  <br />
-  <footer class="footer">
-    <div class="content has-text-centered">
-      <p>
-        Footer
-      </p>
-    </div>
-  </footer>
 </div>
 </template>
 
 <script>
-import ServiceFunnel from './components/ServiceFunnel.vue'
-import ServiceFunnel2 from './components/ServiceFunnel2.vue'
+import Nav from './components/Nav.vue'
+import Footer from './components/Footer.vue'
+import Home from './Home.vue'
+
 
 export default {
   name: 'app',
   components: {
-    ServiceFunnel,
-    ServiceFunnel2
+    Nav,
+    Footer,
+    Home,
+
   }
 }
 </script>
---
+++ service_funnel/src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        Footer
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  methods:{
+    nextStep(value) {
+      this.$router.push('/service_funnel2/'+value);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
